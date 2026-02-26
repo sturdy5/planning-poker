76cdef8275e11378e09a2bd8621632da71a8bf7c
sturdy5 <1645884+sturdy5@users.noreply.github.com>
2026-02-25 23:35:46 -0500

    First attempt

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// In-memory data
+const rooms = new Map();
+
+// Serve all static files (HTML, CSS, JS) from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    socket.on('join-room', ({ roomId, username }) => {
+        socket.join(roomId);
+        if (!rooms.has(roomId)) rooms.set(roomId, { users: {}, showVotes: false });
+        
+        const room = rooms.get(roomId);
+        room.users[socket.id] = { username, vote: null };
+        io.to(roomId).emit('room-update', room);
+    });
+
+    socket.on('submit-vote', ({ roomId, vote }) => {
+        const room = rooms.get(roomId);
+        if (room) {
+            room.users[socket.id].vote = vote;
+            io.to(roomId).emit('room-update', room);
+        }
+    });
+
+    socket.on('reveal-votes', (roomId) => {
+        const room = rooms.get(roomId);
+        if (room) {
+            room.showVotes = true;
+            io.to(roomId).emit('room-update', room);
+        }
+    });
+
+    socket.on('reset-room', (roomId) => {
+        const room = rooms.get(roomId);
+        if (room) {
+            room.showVotes = false;
+            Object.keys(room.users).forEach(id => room.users[id].vote = null);
+            io.to(roomId).emit('room-update', room);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        rooms.forEach((room, roomId) => {
+            if (room.users[socket.id]) {
+                delete room.users[socket.id];
+                if (Object.keys(room.users).length === 0) rooms.delete(roomId);
+                else io.to(roomId).emit('room-update', room);
+            }
+        });
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
